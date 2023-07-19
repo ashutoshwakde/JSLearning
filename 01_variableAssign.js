b14835ceb6749c1caed49a8777e770123e86bed0
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-07-19 16:31:31 +0530

    Basic JavaScript
    Basic JavaScript

---
+++ 01_variableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log("before initialization :" + typeof myFullName);
+myFullName = "Ashutosh Wakde"
+console.log("after initialization :" + myFullName);
+myFullName = "Ashutosh Dnyaneshwar Wakde"
+console.log("after updation :" + myFullName);
+
+
+var myLove;
+myLove = "I love only JS" ;
+console.log(myLove);
+
+var firstName = "Ashutosh" ;
+console.log("First Name :" + firstName);
+
+var lastName = "Wakde" ;
+console.log("Last Name :" + lastName);
+
+var age = 22;
+console.log("age :" + age);
+
+var address = "Shegaon";
+console.log("address:" + address);
+
+var pinCode = 444203 ;
+console.log("pinCode :" + pinCode);
+
+var gender = "male";
+console.log("gender :" + gender);
+
+var isMarried = false;
+console.log("is married :" + isMarried);
---
+++ 02_typesOf.js
@@ -0,0 +1,24 @@
+
+
+var bankName;
+console.log("before initialization :" + bankName);
+bankName = "SBI Bank";
+console.log("after initialization :" + bankName);
+console.log("type of :" + typeof bankName);
+
+var marks = "90%"
+console.log("marks :" + marks);
+console.log("type of :" + typeof marks);
+
+var isWorkingn;
+console.log("before initialization :" + isWorkingn);
+isWorkingn = true;
+console.log("after initialization :" + isWorkingn);
+console.log("type of :" + typeof isWorkingn);
+
+var totalCount ="Hundred and Seven";
+console.log("value is :" +totalCount);
+console.log("type of :" +typeof totalCount);
+totalCount = 107;
+console.log("Changed value is :" +totalCount);
+console.log("type of :" +typeof totalCount);
