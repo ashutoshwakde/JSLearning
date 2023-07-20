aa87e858d5e422c9e3dc9454416bf66b1ecaca66
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-07-20 11:37:18 +0530

    All changes are committed and push
    All changes are committed and push

---
+++ 03_function.js
@@ -0,0 +1,15 @@
+
+
+function swap(n1 ,n2)
+{
+    console.log('Inside swap function');
+    console.log('Before swap=> n1', n1, "n2", n2);
+    var temp = n1;
+    n1 = n2;
+    n2 = temp;
+    console.log('after swap => n1', n1, "n2", n2);
+}
+var num1 = 100;
+var num2 = 200;
+swap(num1, num2);
+console.log('End of program');
