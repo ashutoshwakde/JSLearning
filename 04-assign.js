e0ead256e9b96c5a3e444a029cf70f7c27fb9411
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-07-23 13:24:09 +0530

    All changes are committed and push
    All changes are committed and push

---
+++ 04-assign.js
@@ -0,0 +1,22 @@
+
+
+
+function stringBasic(){
+  var name = "TCS";
+  console.log('my dream company name:', name); 
+}
+stringBasic();
+
+console.log('--------------------------------------------------------');
+
+var hobby1 = "Playing Cricket";
+var hobby2 = "Watching movies";
+var hobby3 = "Painting";
+console.log('My Hobbies are=>', 'hobby1 :', hobby1, 'hobby2:', hobby2, 'hobby3:', hobby3);
+
+console.log('-------------------------------------------------------------------');
+
+var add = hobby1 + hobby3+ hobby3;
+console.log(`sum of total number of charactres:`, add.length);
+
+
---
+++ 04_assign1.js.js
@@ -0,0 +1,21 @@
+
+
+
+function stringBasics(){
+    console.log(`------------------STEP 1------------------`);
+    console.log(`My dream company is: Google`);
+    console.log(`------------------STEP 2------------------`);
+    var num1 = `Playing Cricket`;
+    var num2 = `Reading Books`;
+    var num3 = `Painting`;
+    console.log(`My hobbies are : ${num1} , ${num2} , ${num3}`);
+    console.log(`------------------STEP 3------------------`);
+    var add = num1 + num2 + num3;
+    console.log(`Sum of Total Number of Character is : ${add.length}`);
+}
+stringBasics()
+
+
+
+
+
