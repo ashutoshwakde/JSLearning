ef7074dba3d91a65655696afc393e3848193bae6
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-07-31 10:22:04 +0530

    today assign - control flow
    today assign - control flow

---
+++ 06_for_assignment01.js
@@ -0,0 +1,42 @@
+
+console.log(`------------------print 5 to 15--------------------------`);
+
+for (let index = 5; index <= 15 ; index++) {
+    console.log(index);
+}
+
+console.log(`-------------------print 50 to 40-------------------------`);
+
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+console.log(`-------------------print first 15 odd numbers-------------------------`);
+
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index); 
+}
+
+console.log(`--------------------print first 10 even numbers------------------------`);
+
+for (let index = 0; index < 20; index=index+2) {
+    console.log(index); 
+}
+
+console.log(`---------------------print table of 5-----------------------`);
+
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index); 
+}
+
+console.log(`---------------------print table of 10-----------------------`);
+
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index); 
+}
+
+console.log(`--------------------print table of 10 in reverse order------------------------`);
+
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+}
