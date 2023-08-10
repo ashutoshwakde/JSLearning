59c46ef369fbe34f1b6cc902063d16afc7b58168
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-08-10 13:27:01 +0530

    Assign done
    Assign done

---
+++ 14_primeNumber.js
@@ -0,0 +1,16 @@
+
+
+function test_prime(num) {
+    if (num <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  console.log(test_prime(11));
+  console.log(test_prime(21));
+  
