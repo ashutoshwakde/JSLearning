6a7ef5105f8fdea26a32450a5c8e0b4c05d1c825
Ashutosh Wakde <wakdeashutosh@gmail.com>
2023-08-22 13:33:38 +0530

    Assign 1 done
    Assign 1 done

---
+++ 22arrawFunAssignA.js
@@ -0,0 +1,28 @@
+
+console.log(`-------------------Step 1---------------------`);
+
+let show = function(){
+    console.log(`Good Morning, Today is Tuesday`);
+}
+show();
+
+console.log(`-------------------Step 2---------------------`);
+
+let multiply = (num1, num2, num3=1) => {
+    let mul = num1*num2*num3
+    console.log(`Multiplication is ${mul}`);
+}
+multiply(5, 5, 2);
+multiply(10, 4);
+
+console.log(`-------------------Step 3---------------------`);
+
+let add = (num1,num2,num3,num4,num5)=>{
+    sum = num1+num2+num3+num4+num5
+    console.log(`Sum of elements - ${sum}`);
+}
+add(100,100,200,349,756)
+
+
+
+
